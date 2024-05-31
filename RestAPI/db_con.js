2d4be2c57425d80b8896428e8671aa12c34ed3a8
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-05-31 10:29:20 +0530

    Update db_con.js

--- RestAPI/db_con.js
+++ RestAPI/db_con.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 var db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "theHappyHeads",
-  password: "Aryan1!2@3#",
+  database: "name",
+  password: "pass",
 });
 db.connect(function (err) {
   if (err) throw err;
